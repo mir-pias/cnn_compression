a0e40a723ea4559b179db12067409d70a513d4a0
mir-pias <mir.pias01@gmail.com>
2022-08-01 02:48:17 +0200

    overwrite old conv layers

--- models/TransformLayers/DFT_layers.py
+++ models/TransformLayers/DFT_layers.py
@@ -101,8 +101,10 @@ class LinearDFT(nn.Module):
 class Conv2dDFT(torch.nn.Module):
 
     fcc: torch.nn.Parameter  # central frequencies (output channels)
-    fch: torch.nn.Parameter  # central frequencies (2D convolutional kernel length)
+    fch: torch.nn.Parameter  # central frequencies (2D convolutional kernel height)
+    fcw: torch.nn.Parameter  # central frequencies (2D convolutional kernel width)
     bias: torch.nn.Parameter
+    delta: torch.nn.Parameter  # weights of basis functions
 
     def __init__(
         self,
@@ -125,86 +127,112 @@ class Conv2dDFT(torch.nn.Module):
         self.dilation = _pair(dilation)
         self.groups = groups
 
-        self.register_parameter(
-            name='fcc',
-            param=torch.nn.Parameter(
-                torch.arange(
+
+        fcc = torch.arange(
                     self.out_channels,
                     dtype=torch.get_default_dtype()
                 ).reshape(-1, 1, 1)
-            )
-        )
+
         self.register_parameter(
-            name='fch',
-            param=torch.nn.Parameter(
-                torch.arange(
+            name='fcc',
+            param= torch.nn.Parameter(fcc)
+        )
+
+        fch = torch.arange(
                     self.kernel_size[0],
                     dtype=torch.get_default_dtype()
                 ).reshape(-1, 1, 1)
-            )
+
+        self.register_parameter(
+            name='fch',
+            param= torch.nn.Parameter(fch)
         )
 
-        if bias:
-            self.register_parameter(
-                name='bias',
-                param=torch.nn.Parameter(
-                    torch.normal(
-                        mean=0.0,
-                        std=0.5,
-                        size=(self.out_channels,)
-                    )
-                )
-            )
-        else:
-            self.register_parameter(
-                name='bias',
-                param=None
-            )
+        fcw = torch.arange(
+                    self.kernel_size[1],
+                    dtype=torch.get_default_dtype()
+                ).reshape(-1, 1, 1)
+
+        self.register_parameter(
+            name='fcw',
+            param= torch.nn.Parameter(fcw)
+        )
+
+        self.register_parameter(name='bias', param=torch.nn.Parameter(torch.zeros(self.out_channels)) if bias else None)
+
+        num_filters = self.kernel_size[0] * self.kernel_size[1]
+
+        # delta = torch.full((self.out_channels,num_filters), 1/num_filters)
+        delta = torch.randn(self.out_channels, num_filters)
+
+        self.register_parameter(name='delta', param=torch.nn.Parameter(delta))
 
         def norm(grad):
-            return grad / torch.linalg.norm(grad, ord=None)
+            return grad / (torch.linalg.norm(grad, ord=None) + 1e-8)
 
         self.fcc.register_hook(norm)
         self.fch.register_hook(norm)
+        self.fcw.register_hook(norm)
+        self.delta.register_hook(norm)
 
     def _materialize_weights(self, x: torch.Tensor) -> torch.Tensor:
-        # in_features = x.shape[1]
-        
-
+        in_channels = x.shape[1]
+    
         x_is_complex = x.shape[-1] == 2
 
-        tc = torch.arange(self.in_channels, dtype=x.dtype, device=x.device).reshape(1, -1, 1)
+        tc: torch.Tensor = torch.arange(in_channels, dtype=x.dtype, device=x.device).reshape(1, -1, 1)
 
-        tl = torch.arange(self.kernel_size[0], dtype=x.dtype, device=x.device).reshape(1, -1, 1)
+        th: torch.Tensor = torch.arange(self.kernel_size[0], dtype=x.dtype, device=x.device).reshape(1, -1, 1)
 
-        norm_c = torch.rsqrt(
+        tw: torch.Tensor = torch.arange(self.kernel_size[1], dtype=x.dtype, device=x.device).reshape(1, -1, 1)
+
+        norm_c: torch.Tensor = torch.rsqrt(
             torch.full_like(
-                self.fcc, self.in_channels
-            ) * (
-                torch.ones(self.in_channels, 1, device=x.device, dtype=x.dtype) 
-            )
+                self.fcc, in_channels
+            ) 
         )
 
-        kc: torch.Tensor = norm_c * torch.cat((torch.cos((self.fcc*tc*2*PI)/self.out_channels), - torch.sin((self.fcc*tc*2*PI)/self.out_channels)), dim=-1)
+        kc: torch.Tensor = norm_c * torch.cat((torch.cos((self.fcc*tc*2*PI)/in_channels), 
+                                            - torch.sin((self.fcc*tc*2*PI)/in_channels)), dim=-1)
 
-        norm_l = torch.rsqrt(
+        norm_h: torch.Tensor = torch.rsqrt(
             torch.full_like(
                 self.fch, self.kernel_size[0]
-            ) * (
-                torch.ones(self.kernel_size[0], 1, device=x.device, dtype=x.dtype) 
-            )
+            ) 
         )
         
-        kl: torch.Tensor = norm_l * torch.cat((torch.cos((self.fch*tl*2*PI)/self.kernel_size[0]), - torch.sin((self.fch*tl*2*PI)/self.kernel_size[0])), dim=-1)
+        kh: torch.Tensor = norm_h * torch.cat((torch.cos((self.fch*th*2*PI)/self.kernel_size[0]), 
+                                            - torch.sin((self.fch*th*2*PI)/self.kernel_size[0])), dim=-1)
 
 
-        w: torch.Tensor = kc.reshape(
-        self.out_channels, -1,kc.shape[-1],1,1
-        ) * kl.reshape(
-        1,1,kl.shape[-1], -1, self.kernel_size[0]
+        norm_w: torch.Tensor = torch.rsqrt(
+            torch.full_like(
+                self.fcw, self.kernel_size[1]
+            ) 
         )
+        
+        kw: torch.Tensor = norm_w * torch.cat((torch.cos((self.fcw*tw*2*PI)/self.kernel_size[1]), 
+                                            - torch.sin((self.fcw*tw*2*PI)/self.kernel_size[1])), dim=-1)
 
-        return w.transpose(2,4), x_is_complex ## transpose to fix shape for F.conv2d
+
+
+        w: torch.Tensor =kc.reshape(
+        self.out_channels, -1, kc.shape[-1], 1,1,1,1,1,1
+        )*  kh.reshape(
+        1,1, 1, self.kernel_size[0],-1,kh.shape[-1],1,1,1
+        ) * kw.reshape(1,1,1,1 ,1,1, self.kernel_size[1],-1 ,kw.shape[-1])  # N_out x N_in x 2 x kH x kH x 2 x kW x kW x 2
+
+        w = torch.sum(w,dim=(2,5))  # N_out x N_in x 2 x kH x kH  x kW x kW x 2
+        ## mean doesn't train, sum in complex dims trains
+
+        w = w.reshape(self.out_channels, self.in_channels , self.kernel_size[0], -1, self.kernel_size[1], w.shape[-1] )  
+        # N_out x N_in x kH x (kH * kW) x kW x 2
+        
+        
+        w = torch.mean(w * self.delta.reshape(self.out_channels, 1, 1, -1, 1, 1), dim=3)  # N_out x N_in x kH x kW x 2
+        # w = w.mean(w,dim=3)
+
+        return w, x_is_complex 
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         weights, x_is_complex = self._materialize_weights(x)
@@ -238,7 +266,7 @@ class Conv2dDFT(torch.nn.Module):
 
             y = torch.stack((y0,y1), dim=-1)
 
-        return y.sum(-1) ## no more complex outputs from this layer, maybe ok
+        return y 
 
     def extra_repr(self):
         s = ('in_channels={in_channels}, out_channels={out_channels}, kernel_size={kernel_size}'
--- models/TransformLayers/DST_layers.py
+++ models/TransformLayers/DST_layers.py
@@ -4,7 +4,7 @@ from torch import nn
 import torch.nn.functional as F
 import math
 # noinspection PyProtectedMember
-from torch.nn.modules.utils import _single
+from torch.nn.modules.utils import _single, _pair
 
 from math import pi as PI
 
@@ -78,119 +78,134 @@ class LinearDST(nn.Module):
 ## 2D dst_matrix as conv kernel
 class Conv2dDST(torch.nn.Module):
 
-    fcc: torch.nn.Parameter  # central frequencies (output channels)
-    fcl: torch.nn.Parameter  # central frequencies (2D convolutional kernel length)
+    fcc: torch.nn.Parameter    # central frequencies (output channels)
+    fch: torch.nn.Parameter    # central frequencies (2D convolutional kernel height)
+    fcw: torch.nn.Parameter    # central frequencies (2D convolutional kernel width)
+    delta: torch.nn.Parameter  # weights of basis functions
     bias: torch.nn.Parameter
 
     def __init__(
         self,
-        in_channels: int,
+        in_channels,
         out_channels: int,
-        kernel_size: Union[int, Tuple[int]],
-        stride: Union[int, Tuple[int]] = 1,
-        padding: Optional[Union[int, Tuple[int]]] = 0,
-        dilation: Union[int, Tuple[int]] = 1,
+        kernel_size: Union[int, Tuple[int, int]],
+        stride: Union[int, Tuple[int, int]] = 1,
+        padding: Optional[Union[int, Tuple[int, int]]] = 0,
+        dilation: Union[int, Tuple[int, int]] = 1,
         groups: int = 1,
         bias: bool = True
     ):
         super(Conv2dDST, self).__init__()
-        
+
         self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.kernel_size = _single(kernel_size)
-        self.stride = _single(stride)
-        self.padding = _single(padding)
-        self.dilation = _single(dilation)
+        self.out_channels = out_channels    
+        self.kernel_size = _pair(kernel_size)
+        self.stride = _pair(stride)
+        self.padding = _pair(padding)
+        self.dilation = _pair(dilation)
         self.groups = groups
 
-        self.register_parameter(
-            name='fcc',
-            param=torch.nn.Parameter(
-                torch.arange(
-                    self.out_channels,
-                    dtype=torch.get_default_dtype()
-                ).reshape(-1, 1)
-            )
-        )
-        self.register_parameter(
-            name='fcl',
-            param=torch.nn.Parameter(
-                torch.arange(
-                    self.kernel_size[0],
-                    dtype=torch.get_default_dtype()
-                ).reshape(-1, 1)
-            )
-        )
+        fcc = torch.arange(
+            self.out_channels,
+            dtype=torch.get_default_dtype()
+        ).reshape(-1, 1)
+        self.register_parameter(name='fcc', param=torch.nn.Parameter(fcc))
 
-        if bias:
-            self.register_parameter(
-                name='bias',
-                param=torch.nn.Parameter(
-                    torch.normal(
-                        mean=0.0,
-                        std=0.5,
-                        size=(self.out_channels,)
-                    )
-                )
-            )
-        else:
-            self.register_parameter(
-                name='bias',
-                param=None
-            )
+        fch = torch.arange(
+            self.kernel_size[0],
+            dtype=torch.get_default_dtype()
+        ).reshape(-1, 1)
+        self.register_parameter(name='fch', param=torch.nn.Parameter(fch))
+
+        fcw = torch.arange(
+            self.kernel_size[1],
+            dtype=torch.get_default_dtype()
+        ).reshape(-1, 1)
+        self.register_parameter(name='fcw', param=torch.nn.Parameter(fcw))
+
+        num_filters = self.kernel_size[0] * self.kernel_size[1]
+        
+        delta = torch.randn(self.out_channels, num_filters)
+        
+        self.register_parameter(name='delta', param=torch.nn.Parameter(delta))
+
+        self.register_parameter(name='bias', param=torch.nn.Parameter(torch.zeros(self.out_channels)) if bias else None)
 
         def norm(grad):
-            return grad / torch.linalg.norm(grad, ord=None)
+            return grad / (torch.linalg.norm(grad, ord=None) + 1e-8)
 
         self.fcc.register_hook(norm)
-        self.fcl.register_hook(norm)
+        self.fch.register_hook(norm)
+        self.fcw.register_hook(norm)
+
 
     def _materialize_weights(self, x: torch.Tensor) -> torch.Tensor:
-        # in_features = x.shape[1]
+        # in_channels: int = x.shape[1] // self.groups
+        in_channels = self.in_channels // self.groups
 
-        try:
-            width = self.kernel_size[1]
-        except IndexError:
-            width = self.kernel_size[0]
+        tc: torch.Tensor = torch.arange(
+            in_channels,
+            dtype=x.dtype,
+            device=x.device
+        ).reshape(
+            1,  # feature dimension
+            -1  # time dimension
+        )
+        th: torch.Tensor = torch.arange(
+            self.kernel_size[0], dtype=x.dtype, device=x.device
+        ).reshape(1, -1)
+        tw: torch.Tensor = torch.arange(
+            self.kernel_size[1], dtype=x.dtype, device=x.device
+        ).reshape(1, -1)
 
-        t_c = torch.arange(self.in_channels, dtype=x.dtype, device=x.device).reshape(1, -1)
-
-        t_l = torch.arange(width, dtype=x.dtype, device=x.device).reshape(1, -1)
-
-        norm_c = torch.rsqrt(
+        norm_c: torch.Tensor = torch.rsqrt(
             torch.full_like(
-                self.fcc, 2 * self.out_channels
+                self.fcc, 2 * in_channels
             ) * (
                 torch.eye(self.out_channels, 1, device=x.device, dtype=x.dtype) + 1
             )
-        ) * torch.rsqrt(torch.full_like(
-                self.fcc, 2))
+        ) 
 
-        kc: torch.Tensor = 2 * norm_c * torch.sin(0.5 * PI * (self.fcc + 1) * (2 * t_c + 1) / self.out_channels)
+        kc: torch.Tensor = 2 * norm_c * torch.sin(0.5 * PI * ((self.fcc + 1 )/ in_channels) * (2 * tc + 1)) 
 
-        norm_l = torch.rsqrt(
+        norm_h: torch.Tensor = torch.rsqrt(
             torch.full_like(
-                self.fcl, 2 * self.kernel_size[0]
+                self.fch, 2 * self.kernel_size[0]
             ) * (
                 torch.eye(self.kernel_size[0], 1, device=x.device, dtype=x.dtype) + 1
             )
-        ) * torch.rsqrt(torch.full_like(
-                self.fcl, 2))
+        ) 
+        kh: torch.Tensor = 2 * norm_h * torch.sin(0.5 * PI * ((self.fch +1) / self.kernel_size[0]) * (2 * th + 1)) 
 
-        kl: torch.Tensor = 2 * norm_l * torch.sin(0.5 * PI * (self.fcl + 1) * (2 * t_l + 1) / self.kernel_size[0])
+        norm_w: torch.Tensor = torch.rsqrt(
+            torch.full_like(
+                self.fcw, 2 * self.kernel_size[1]
+            ) * (
+                torch.eye(self.kernel_size[1], 1, device=x.device, dtype=x.dtype) + 1
+            )
+        ) 
+        kw: torch.Tensor = 2 * norm_w * torch.sin(0.5 * PI * ((self.fcw +1) / self.kernel_size[1]) * (2 * tw + 1)) 
+
 
         w: torch.Tensor = kc.reshape(
-            self.out_channels, -1, 1,1
-        ) * kl.reshape(
-            1,1, -1, width
+            self.out_channels, -1, 1, 1, 1, 1
+        ) * kh.reshape(
+            1,1, self.kernel_size[0], -1, 1, 1
+        ) * kw.reshape(
+            1, 1, 1, 1, -1, self.kernel_size[1]                               # N_out x N_in x kH x kH x kW x kW
         )
+        
+        w = w.reshape(self.out_channels, in_channels, -1, *self.kernel_size)  # N_out x N_in x kH x (kH * kW) x kW
 
-        return w
+        # weighted sum of basis functions
+        w = torch.mean(w * self.delta.reshape(self.out_channels, 1, -1, 1, 1), dim=2)  # N_out x N_in x kH x kW
+        # w = torch.mean(w,dim=2)
+        return w 
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         w = self._materialize_weights(x)
 
-        return F.conv2d(
+        y = F.conv2d(
             input=x,
             weight=w,
             bias=self.bias,
@@ -199,6 +214,8 @@ class Conv2dDST(torch.nn.Module):
             dilation=self.dilation,
             groups=self.groups
         )
+
+        return y
 
     def extra_repr(self):
         s = ('in_channels={in_channels}, out_channels={out_channels}, kernel_size={kernel_size}'

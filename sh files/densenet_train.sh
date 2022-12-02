38559a0593f71c49524ba29b49959b93e8164878
mir-pias <mir.pias01@gmail.com>
2022-12-02 17:27:46 +0100

    sh files

---
+++ sh files/densenet_train.sh
@@ -0,0 +1,198 @@
+python src/main.py --model=DenseNet121 --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='dct' --layers='fc' --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='dct' --layers='conv' --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='dct' --layers='all' --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='dst' --layers='fc' --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='dst' --layers='conv' --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='dst' --layers='all'  --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='dft' --layers='fc' --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='dft' --layers='conv' --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='dft' --layers='all' --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='realshannon' --layers='fc' --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='realshannon' --layers='conv' --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='realshannon' --layers='all' --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='shannon' --layers='fc' --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='shannon' --layers='conv' --dataset='cifar10'
+
+python src/main.py --model=DenseNet121 --kernel='shannon' --layers='all' --dataset='cifar10'
+
+
+python src/main.py --model=DenseNet121 --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='dct' --layers='fc' --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='dct' --layers='conv' --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='dct' --layers='all' --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='dst' --layers='fc' --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='dst' --layers='conv' --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='dst' --layers='all'  --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='dft' --layers='fc' --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='dft' --layers='conv' --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='dft' --layers='all' --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='realshannon' --layers='fc' --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='realshannon' --layers='conv' --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='realshannon' --layers='all' --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='shannon' --layers='fc' --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='shannon' --layers='conv' --dataset='mnist'
+
+python src/main.py --model=DenseNet121 --kernel='shannon' --layers='all' --dataset='mnist'
+
+
+python src/main.py --model=DenseNet121 --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='dct' --layers='fc' --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='dct' --layers='conv' --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='dct' --layers='all' --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='dst' --layers='fc' --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='dst' --layers='conv' --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='dst' --layers='all'  --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='dft' --layers='fc' --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='dft' --layers='conv' --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='dft' --layers='all' --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='realshannon' --layers='fc' --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='realshannon' --layers='conv' --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='realshannon' --layers='all' --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='shannon' --layers='fc' --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='shannon' --layers='conv' --dataset='cifar100'
+
+python src/main.py --model=DenseNet121 --kernel='shannon' --layers='all' --dataset='cifar100'
+
+
+python src/main.py --model=DenseNet201 --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='dct' --layers='fc' --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='dct' --layers='conv' --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='dct' --layers='all' --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='dst' --layers='fc' --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='dst' --layers='conv' --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='dst' --layers='all'  --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='dft' --layers='fc' --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='dft' --layers='conv' --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='dft' --layers='all' --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='realshannon' --layers='fc' --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='realshannon' --layers='conv' --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='realshannon' --layers='all' --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='shannon' --layers='fc' --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='shannon' --layers='conv' --dataset='cifar10'
+
+python src/main.py --model=DenseNet201 --kernel='shannon' --layers='all' --dataset='cifar10'
+
+
+
+
+python src/main.py --model=DenseNet201 --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='dct' --layers='fc' --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='dct' --layers='conv' --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='dct' --layers='all' --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='dst' --layers='fc' --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='dst' --layers='conv' --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='dst' --layers='all'  --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='dft' --layers='fc' --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='dft' --layers='conv' --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='dft' --layers='all' --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='realshannon' --layers='fc' --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='realshannon' --layers='conv' --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='realshannon' --layers='all' --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='shannon' --layers='fc' --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='shannon' --layers='conv' --dataset='mnist'
+
+python src/main.py --model=DenseNet201 --kernel='shannon' --layers='all' --dataset='mnist'
+
+
+python src/main.py --model=DenseNet201 --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='dct' --layers='fc' --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='dct' --layers='conv' --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='dct' --layers='all' --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='dst' --layers='fc' --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='dst' --layers='conv' --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='dst' --layers='all'  --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='dft' --layers='fc' --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='dft' --layers='conv' --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='dft' --layers='all' --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='realshannon' --layers='fc' --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='realshannon' --layers='conv' --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='realshannon' --layers='all' --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='shannon' --layers='fc' --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='shannon' --layers='conv' --dataset='cifar100'
+
+python src/main.py --model=DenseNet201 --kernel='shannon' --layers='all' --dataset='cifar100'
